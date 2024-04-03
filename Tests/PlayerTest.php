810d0e187331ccb644d6770875558289645cdb7a
Tom <cartier.tom49@gmail.com>
2024-04-03 12:20:29 +0200

    ✅ Add player

--- Tests/PlayerTest.php
+++ Tests/PlayerTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use App\Classes\CardProperty;
 use PHPUnit\Framework\TestCase;
 
 use App\Classes\Player;
+use App\Services\BtpService;
 
 final class PlayerTest extends TestCase
 {
@@ -17,5 +19,60 @@ final class PlayerTest extends TestCase
         $playerName = $player->getName();
         // le nom du joueur générée est bien de type "string"
         $this->assertTrue(gettype($playerName) == "string");
+    }
+
+    public function testGetBalance(): void
+    {
+        $player = new Player('Tom');
+        $balance = $player->getBalance();
+        // le solde du joueur est bien de type "integer"
+        $this->assertTrue(gettype($balance) == "integer");
+    }
+
+    public function testAddMoney(): void
+    {
+        $player = new Player('Tom');
+        $initalBalance = $player->getBalance();
+
+        $player->addMoney(1000);
+
+        $this->assertEquals($initalBalance + 1000, $player->getBalance());
+    }
+
+    public function testRemoveMoney(): void
+    {
+        $player = new Player('Tom');
+        $initalBalance = $player->getBalance();
+
+        $player->removeMoney(1000);
+
+        $this->assertEquals($initalBalance - 1000, $player->getBalance());
+    }
+
+    public function testGetProperties(): void
+    {
+        $player = new Player('Tom');
+        $properties = $player->getProperties();
+        // les properties du joueur est bien de type "array"
+        $this->assertTrue(gettype($properties) == "array");
+    }
+
+    public function testAddProperties(): void
+    {
+        $player = new Player('Tom');
+        $initialProperties = $player->getProperties();
+
+        $BtpService = new BtpService();
+        $property = new CardProperty('Dark blue', 'Rue de la Paix', 400000000, 1000, $BtpService);
+
+        // Ajout de la propriété sur le joueur
+        $player->addProperty($property);
+
+        // Vérifier que la propriété à bien été ajoutée
+        $updatedProperties = $player->getProperties();
+        $this->assertContains($property, $updatedProperties);
+
+        // Vérifier que la liste des propriétés contient une propriété de plus
+        $this->assertCount(count($initialProperties) + 1, $updatedProperties);
     }
 }
--- src/Classes/Player.php
+++ src/Classes/Player.php
@@ -8,15 +8,48 @@ class Player implements InterfacePlayer
 {
     private string $name;
     private int $balance;
+    private array $properties;
 
     public function __construct($name)
     {
         $this->name = $name;
         $this->balance = 5000000;
+        $this->properties = [];
     }
 
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function getBalance(): int
+    {
+        return $this->balance;
+    }
+
+    public function addMoney(int $amount): void
+    {
+        $this->balance += $amount;
+    }
+
+    public function removeMoney(int $amount): void
+    {
+        $newBalance = $this->balance - $amount;
+
+        if ($newBalance < 0) {
+            throw new \RuntimeException('Solde insuffisant');
+        }
+
+        $this->balance = $newBalance;
+    }
+
+    public function getProperties(): array
+    {
+        return $this->properties;
+    }
+
+    public function addProperty(CardProperty $property): void
+    {
+        $this->properties[] = $property;
     }
 }
